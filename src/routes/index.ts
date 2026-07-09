246c78f7c251497f2e5543ff07163befae716ec3
bearxmx <bearxmx@outlook.com>
2026-07-09 16:15:20 +0800

    合并冲突

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -55,10 +55,7 @@ export const routes = [
     meta: {
       title: '找邻居',
     },
-<<<<<<< HEAD
     component: () => import('@/views/find-neighborhood/index.vue'),
-=======
-    component: () => import('@/views/61-find-neighborhood/index.vue'),
   },
   {
     path: '/threeD-terrain',
@@ -67,7 +64,6 @@ export const routes = [
       title: '3D等高线地形图',
     },
     component: () => import('@/views/threeD-terrain/index.vue'),
->>>>>>> 044d9750181fe5c4459d8df5dd17a2a1deeb1c7c
   },
 ]
 
