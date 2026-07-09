044d9750181fe5c4459d8df5dd17a2a1deeb1c7c
oy <oyangml@163.com>
2026-07-09 14:50:20 +0800

    3D地形与等高线地形图

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -56,7 +56,15 @@ export const routes = [
       title: '六上一起做-找邻居',
     },
     component: () => import('@/views/61-find-neighborhood/index.vue'),
-  }
+  },
+  {
+    path: '/threeD-terrain',
+    name: '3D-terrain',
+    meta: {
+      title: '3D等高线地形图',
+    },
+    component: () => import('@/views/threeD-terrain/index.vue'),
+  },
 ]
 
 export const routesNav = routes.map((route) => {
